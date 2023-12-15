d363082102dc4da9f860760d44416bc5ce871c7b
edhoguntur <edho.guntur@gmail.com>
2023-12-15 11:59:39 +0700

    Done CS193p Lecture 4 We could shuffle the deck of cards

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,6 +7,34 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    var model: MemorizeGame<String>
+
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "💩", "🐓", "🐑", "🐇", "🌮", "🍔", "🍅", "🍒", "🍉"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 16) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
 }
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,36 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Edho Guntur Adhitama on 15/12/23.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        // add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
+    struct Card {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        let content: CardContent
+    }
+}
